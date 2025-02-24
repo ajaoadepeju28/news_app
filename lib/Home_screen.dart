996743ffd2db937a794e6bddf25cf4052333b469
ajaoadepeju28 <ajaoferanmi08@gmail.com>
2025-02-24 06:23:03 -0800

    finish

---
+++ lib/Home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/news_page.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'WELCOME',
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontFamily: 'Roboto',
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'to',
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontFamily: 'Times New Roman',
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontFamily: 'Times New Roman',
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const NewsPage()));
+                },
+                child: Text(
+                  'click here',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.blueAccent),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Home_screen.dart';
 import 'package:news_app/news_page.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'NewsApp',
-      home: NewsPage(),
+      home:HomeScreen(),
     );
   }
 }
